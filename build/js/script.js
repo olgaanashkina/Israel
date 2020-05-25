f0ca07e01cbb6792d8f2c4566b5061c208137b1d
olgaanashkina <olena_555@mail.ru>
2020-05-25 10:22:57 +0300

    Добавляет остальные блоки

--- build/js/script.js
+++ build/js/script.js
@@ -16,3 +16,39 @@ tabs.forEach(function(tab) {
     content.classList.add('is-active');
   });
 });
+
+var accordions = document.getElementsByClassName("accordion");
+var open = document.getElementsByClassName('accordion-open');
+for (var i = 0; i < accordions.length; i++) {
+  accordions[i].onclick = function() {
+    if (open.length > 0 && open[0] !== this)
+      open[0].classList.remove('accordion-open');
+      this.classList.toggle('accordion-open');
+  }
+}
+
+var onToggleSlider = function(classNameSlide, classNameButton) {
+  var active = 0, prev = 0;
+  var slides = document.getElementsByClassName(classNameSlide);
+  var arrows = document.getElementsByClassName(classNameButton);
+
+  for(var i=0; i<arrows.length; i++) {
+    arrows[i].addEventListener('click', function(e) {
+      prev = active;
+      if(!!~e.target.classList[1].indexOf('left')) {
+        active-1 >= 0 ? --active : active = slides.length-1;
+      } else {
+        active+1 <= slides.length-1 ? ++active : active = 0;
+      }
+      changeActiveSlide(prev);
+    });
+  }
+  var changeActiveSlide = function(prev) {
+    slides[prev].classList.remove('show');
+    slides[active].classList.add('show');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  onToggleSlider('comments__slide', 'comments__button');
+});
