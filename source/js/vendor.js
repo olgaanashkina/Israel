8c4cb142e4fb7d6e4439e32c7f23b304a8fdab93
olgaanashkina <olena_555@mail.ru>
2020-06-02 09:13:10 +0300

    Добавляет правки

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,6 @@
+// Picture element HTML5 shiv
+document.createElement( "picture" );
+
 /*! picturefill - v3.0.2 - 2016-02-12
  * https://scottjehl.github.io/picturefill/
  * Copyright (c) 2016 https://github.com/scottjehl/picturefill/blob/master/Authors.txt; Licensed MIT
